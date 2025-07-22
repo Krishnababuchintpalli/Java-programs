e554c3e5cc20860068550e97334367ab5dd2b633
Krishna Babu <139860669+Krishnababuchintpalli@users.noreply.github.com>
2025-07-22 15:57:27 +0530

    Add files via upload

---
+++ OopsPrograms/Inheritance.java
@@ -0,0 +1,22 @@
+package OopsPrograms;
+
+public class Inheritance {
+	public static void main(String[] args) {
+	       Car c = new Car();
+	       c.start();  // from Vehicle
+	       c.drive();  // from Car
+	   }
+
+
+}
+class Vehicle {
+	   void start() {
+	       System.out.println("Vehicle started");
+	   }
+	}
+	class Car extends Vehicle {
+	   void drive() {
+	       System.out.println("Car is driving");
+	   }
+	}
+
